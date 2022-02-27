dff3a35714622ef520e66a33116f76e745ffd356
Lakshmi Phani Ganesh <surepalli.ganesh@gmail.com>
2022-02-27 10:21:15 -0500

    Final Changes

--- app/src/main/java/com/rbc/rbcmobile/Home.kt
+++ app/src/main/java/com/rbc/rbcmobile/Home.kt
@@ -1,88 +1,76 @@
-package com.rbc.rbcmobile;
+package com.rbc.rbcmobile
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.app.AppCompatActivity
+import com.rbc.rbcmobile.viewmodel.AccountViewModel
+import androidx.recyclerview.widget.RecyclerView
+import com.rbc.rbcmobile.adapter.AccountRecyclerAdapter
+import androidx.recyclerview.widget.GridLayoutManager
+import android.os.Bundle
+import com.rbc.rbcmobile.databinding.ActivityHomeBinding
+import com.rbc.rbcmobile.model.AccountModel
+import java.util.*
 
-import android.os.Bundle;
+class Home : AppCompatActivity() {
+    private var binding: ActivityHomeBinding? = null
+    private var accountViewModel: AccountViewModel? = null
 
-import com.rbc.rbcmobile.adapter.AccountRecyclerAdapter;
-import com.rbc.rbcmobile.databinding.ActivityHomeBinding;
-import com.rbc.rbcmobile.model.AccountModel;
-import com.rbc.rbcmobile.viewmodel.AccountViewModel;
+    private var recyclerView: RecyclerView? = null
+    private var accountRecyclerAdapter: AccountRecyclerAdapter? = null
+    private var gridLayoutManager: GridLayoutManager? = null
 
-import java.util.Calendar;
-import java.util.List;
-
-public class Home extends AppCompatActivity {
-
-    ActivityHomeBinding binding;
-
-    AccountViewModel accountViewModel;
-
-    //Recycler View Variables
-    RecyclerView recyclerView;
-    AccountRecyclerAdapter accountRecyclerAdapter;
-    GridLayoutManager gridLayoutManager;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        this.binding = ActivityHomeBinding.inflate(getLayoutInflater());
-        setContentView(this.binding.getRoot());
-
-        setWelcomeMessage();
-
-        this.accountViewModel = AccountViewModel.getInstance(getApplication());
-
-        observeLiveDataFromVM();
-        getNewList();
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding!!.root)
+        setWelcomeMessage()
+        accountViewModel = AccountViewModel.getInstance(application)
+        observeLiveDataFromVM()
+        newList
     }
 
-    private void getNewList() {
-        this.accountViewModel.getAllAccountsList();
+    private val newList: Unit
+        get() {
+            accountViewModel!!.getAllAccountsList()
+        }
+
+    private fun observeLiveDataFromVM() {
+        accountViewModel!!.mutableLiveDataAccounts.observe(
+            this,
+            { accountModels -> startRecyclerView(accountModels) })
     }
 
-    private void observeLiveDataFromVM() {
-        this.accountViewModel.mutableLiveDataAccounts.observe(this, new Observer<List<AccountModel>>() {
-            @Override
-            public void onChanged(List<AccountModel> accountModels) {
-                startRecyclerView(accountModels);
+    private fun startRecyclerView(accountModels: List<AccountModel>) {
+        recyclerView = binding!!.homeRecyclerView
+        gridLayoutManager = GridLayoutManager(applicationContext, 1)
+        recyclerView!!.layoutManager = gridLayoutManager
+        accountRecyclerAdapter = AccountRecyclerAdapter(this, accountModels)
+        recyclerView!!.adapter = accountRecyclerAdapter
+    }
+
+    private fun setWelcomeMessage() {
+        val calendar = Calendar.getInstance()
+        val timeOfDay = calendar[Calendar.HOUR_OF_DAY]
+        var title = "Welcome"
+        if (timeOfDay in 1..3) {
+            title = "Hello, Night Owl"
+            binding!!.homeWelcomeMessage.text = title
+        }
+        when (timeOfDay) {
+            in 4..11 -> {
+                title = "Good Morning"
+                binding!!.homeWelcomeMessage.text = title
             }
-        });
-    }
-
-    private void startRecyclerView(List<AccountModel> accountModels) {
-        this.recyclerView = this.binding.homeRecyclerView;
-        gridLayoutManager = new GridLayoutManager(getApplicationContext(), 1);
-        recyclerView.setLayoutManager(gridLayoutManager);
-        accountRecyclerAdapter = new AccountRecyclerAdapter(this, accountModels);
-        recyclerView.setAdapter(accountRecyclerAdapter);
-    }
-
-    private void setWelcomeMessage() {
-        Calendar calendar = Calendar.getInstance();
-        int timeOfDay = calendar.get(Calendar.HOUR_OF_DAY);
-
-        String title = "Welcome";
-
-        if(timeOfDay > 0 && timeOfDay < 4){
-            title = "Hello, Night Owl";
-            this.binding.homeWelcomeMessage.setText(title);
-        }if(timeOfDay >= 4 && timeOfDay < 12){
-            title = "Good Morning";
-            this.binding.homeWelcomeMessage.setText(title);
-        }else if(timeOfDay >= 12 && timeOfDay < 16){
-            title = "Good Afternoon";
-            this.binding.homeWelcomeMessage.setText(title);
-        }else if(timeOfDay >= 16 && timeOfDay < 24){
-            title = "Good Evening";
-            this.binding.homeWelcomeMessage.setText(title);
-        }else{
-            this.binding.homeWelcomeMessage.setText(title);
+            in 12..15 -> {
+                title = "Good Afternoon"
+                binding!!.homeWelcomeMessage.text = title
+            }
+            in 16..23 -> {
+                title = "Good Evening"
+                binding!!.homeWelcomeMessage.text = title
+            }
+            else -> {
+                binding!!.homeWelcomeMessage.text = title
+            }
         }
     }
-
 }
